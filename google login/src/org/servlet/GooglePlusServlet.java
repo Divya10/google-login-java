81cea30acc02fe5ae9dd9c048b0d782a5a2e93fd
Divya10 <divya.agarwal24@outlook.com>
2016-12-26 01:56:36 +0530

    commit

---
+++ google login/src/org/servlet/GooglePlusServlet.java
@@ -0,0 +1,48 @@
+package org.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GooglePlusServlet
+ */
+@WebServlet("/googleplus")
+public class GooglePlusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GooglePlusServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+       private static final String CLIENT_ID = ":-  50376313406-hp2i1too98fc8nbgpd9cd0bvs0j4s0qj.apps.googleusercontent.com"; 
+       private static final String CLIENT_SECRET = "WB2FlaMS5JprvrvI3IHnBT5x";
+       @Override 
+       protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+          throws IOException, ServletException {
+          //Configure 
+          ServiceBuilder builder= new ServiceBuilder(); 
+          OAuthService service = builder.provider(Google2Api.class) 
+             .apiKey(CLIENT_ID) 
+             .apiSecret(CLIENT_SECRET) 
+             .callback("http://localhost:8080/mywebapp/oauth2callback&quot")
+             .scope("openid profile email " + 
+                   "https://www.googleapis.com/auth/plus.login " + 
+                   “https://www.googleapis.com/auth/plus.me”)  
+             .debug(System.out) 
+             .build(); //Now build the call
+          HttpSession sess = req.getSession(); 
+          sess.setAttribute("oauth2Service", service);
+          resp.sendRedirect(service.getAuthorizationUrl(null)); 
+       } 
+    } 
+
+
